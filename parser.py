6e906a08e797cc262584b9b1e629fdba70e9a75c
tpltnt <tpltnt.github@dropcut.net>
2013-03-10 21:57:36 +0100

    usage added

--- parser.py
+++ parser.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
-import xml.etree.ElementTree
+import sys
+import xml.etree.ElementTree as ET
+
+def usage():
+    print("usage: ./parser.py <NETXML-FILE>")
+
+if 2 != len(sys.argv):
+    usage()
+    sys.exit(1)
