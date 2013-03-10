72abfa5a91a997bf7af248cdffc9329165e2195e
tpltnt <tpltnt.github@dropcut.net>
2013-03-10 23:07:33 +0100

    file opening handled

--- parser.py
+++ parser.py
@@ -11,3 +11,10 @@ if __name__ == '__main__':
     if 2 != len(sys.argv):
         usage()
         sys.exit(1)
+
+    tree = ET.ElementTree()
+    try:
+        tree = ET.parse(sys.argv[1])
+    except FileNotFoundError:
+        sys.stderr.write("opening file failed\n")
+    root = tree.getroot()
