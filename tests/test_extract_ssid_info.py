8006175d19f53b84a8606bc813993bace226a96a
tpltnt <tpltnt.github@dropcut.net>
2013-03-23 23:13:25 +0100

    pep8 fixes

--- tests/test_extract_ssid_info.py
+++ tests/test_extract_ssid_info.py
@@ -3,34 +3,43 @@ sys.path.append('../mantis')
 import pytest
 from kismetlogparser import *
 
+
 def test_empty(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.extract_ssid_info()
+
 
 def test_float(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.extract_ssid_info(2.3)
 
+
 def test_int(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.extract_ssid_info(42)
+
 
 def test_string(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.extract_ssid_info('foo')
 
-def test_minimalfile(plainmantis,et_ssidnode):
-    info = {'max-rate': 54.0, 'essid': 'WLAN', 'encryption': ['WPA+TKIP', 'WPA+PSK']}
+
+def test_minimalfile(plainmantis, et_ssidnode):
+    info = {'max-rate': 54.0, 'essid': 'WLAN',
+            'encryption': ['WPA+TKIP', 'WPA+PSK']}
     assert info == plainmantis.extract_ssid_info(et_ssidnode)
+
 
 def test_emptyfile(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.extract_ssid_info('./tests/empty.xml')
 
-def test_empty_et(plainmantis,et_empty):
+
+def test_empty_et(plainmantis, et_empty):
     with pytest.raises(TypeError):
         plainmantis.extract_ssid_info(et_empty)
 
-def test_empty_et(plainmantis,et_one_node):
+
+def test_empty_et(plainmantis, et_one_node):
     with pytest.raises(AttributeError):
         plainmantis.extract_ssid_info(et_one_node)
--- tests/test_parse_xml.py
+++ tests/test_parse_xml.py
@@ -3,21 +3,26 @@ sys.path.append('../mantis')
 import pytest
 from kismetlogparser import *
 
+
 def test_empty(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.parse_xml()
+
 
 def test_float(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.parse_xml(2.3)
 
+
 def test_int(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.parse_xml(42)
 
+
 def test_nonexistent(plainmantis):
     with pytest.raises(ValueError):
         plainmantis.parse_xml('/i/do/not/exist')
+
 
 def test_minimalfile(plainmantis):
     assert 1 == plainmantis.parse_xml('./tests/minimal.netxml')
