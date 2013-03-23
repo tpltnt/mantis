e2fa826714539123ac3e839bdee0dbb430451af2
tpltnt <tpltnt.github@dropcut.net>
2013-03-23 16:38:13 +0100

    unused credentials removed

--- tests/test_parse_xml.py
+++ tests/test_parse_xml.py
@@ -3,13 +3,9 @@ sys.path.append('../mantis')
 import pytest
 from kismetlogparser import *
 
-# adjust accordung to your configuration
-USERNAME = 'username'
-PASSWORD = 'password'
-
 @pytest.fixture
 def mantis():
-    return Mantis(username=USERNAME,password=PASSWORD)
+    return Mantis()
 
 def test_empty(mantis):
     with pytest.raises(TypeError):
