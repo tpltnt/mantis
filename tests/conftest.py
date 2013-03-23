ca2129d964c9279a23caf57688d50a0d8199672d
tpltnt <tpltnt.github@dropcut.net>
2013-03-23 16:59:02 +0100

    XML SSID node fixture added

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,15 @@ from kismetlogparser import *
 @pytest.fixture(scope="module")
 def plainmantis():
     return Mantis()
+
+@pytest.fixture(scope="module")
+def et_ssidnode(mantis):
+    foomantis = Mantis()
+    tree = ET.ElementTree()
+    try:
+        tree = ET.parse('./tests/minimal.netxml')
+    except FileNotFoundError:
+        raise FileNotFoundError("netxml file not found")
+    root = tree.getroot()
+    network = tree.findall('wireless-network')[1]
+    return network.find('SSID')
