0228395a4f7796ee987c236ed9ac45f94d6cf436
tpltnt <tpltnt.github@dropcut.net>
2013-03-23 22:01:45 +0100

    test_extract_ssid_data -> test_extract_ssid_info

--- tests/test_extract_ssid_info.py
+++ tests/test_extract_ssid_info.py
@@ -5,32 +5,32 @@ from kismetlogparser import *
 
 def test_empty(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data()
+        plainmantis.extract_ssid_info()
 
 def test_float(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data(2.3)
+        plainmantis.extract_ssid_info(2.3)
 
 def test_int(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data(42)
+        plainmantis.extract_ssid_info(42)
 
 def test_string(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data('foo')
+        plainmantis.extract_ssid_info('foo')
 
 def test_minimalfile(plainmantis,et_ssidnode):
-    data = {'max-rate': 54.0, 'essid': 'WLAN', 'encryption': ['WPA+TKIP', 'WPA+PSK']}
-    assert data == plainmantis.extract_ssid_data(et_ssidnode)
+    info = {'max-rate': 54.0, 'essid': 'WLAN', 'encryption': ['WPA+TKIP', 'WPA+PSK']}
+    assert info == plainmantis.extract_ssid_info(et_ssidnode)
 
 def test_emptyfile(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data('./tests/empty.xml')
+        plainmantis.extract_ssid_info('./tests/empty.xml')
 
 def test_empty_et(plainmantis,et_empty):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data(et_empty)
+        plainmantis.extract_ssid_info(et_empty)
 
 def test_empty_et(plainmantis,et_one_node):
     with pytest.raises(AttributeError):
-        plainmantis.extract_ssid_data(et_one_node)
+        plainmantis.extract_ssid_info(et_one_node)
