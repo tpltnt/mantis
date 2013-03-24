29b405aff4e81872ce84a3c2437de60bca0dd443
tpltnt <tpltnt.github@dropcut.net>
2013-03-24 10:34:16 +0100

    deflate tests added

--- tests/test_deflate.py
+++ tests/test_deflate.py
@@ -3,13 +3,20 @@ sys.path.append('../mantis')
 import pytest
 from kismetlogparser import *
 
-pytest.fail("not implmenented")
+
+def test_plain_empty(plainmantis):
+    with pytest.raises(BaseException):
+        plainmantis.deflate()
 
 
-def test_empty(plainmantis):
-    plainmantis.deflate()
-
-
-def test_string(plainmantis):
+def test_plain_string(plainmantis):
     with pytest.raises(TypeError):
         plainmantis.deflate('foo')
+
+#def test_auth_empty(authmantis):
+#    authmantis.deflate()
+
+
+def test_auth_string(authmantis):
+    with pytest.raises(TypeError):
+        authmantis.deflate('foo')
