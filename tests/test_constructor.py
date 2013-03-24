3fd421300a3b9a0b1fa50698e9343ce4eec9f9a7
tpltnt <tpltnt.github@dropcut.net>
2013-03-24 09:10:31 +0100

    pep8 fixes

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -3,38 +3,48 @@ sys.path.append('../mantis')
 import pytest
 from kismetlogparser import *
 
+
 # adjust accordung to your configuration
 USERNAME = 'username'
 PASSWORD = 'password'
 
+
 def test_contructor_empty():
     foo = Mantis()
+
 
 def test_constructor_invalid_keyword():
     with pytest.raises(TypeError):
         foo = Mantis(bar='baz')
 
+
 def test_username_and_password():
-    foo = Mantis(username=USERNAME,password=PASSWORD)
+    foo = Mantis(username=USERNAME, password=PASSWORD)
+
 
 def test_username_only():
     with pytest.raises(ValueError):
         foo = Mantis(username=USERNAME)
 
+
 def test_password_only():
     with pytest.raises(ValueError):
         foo = Mantis(password=PASSWORD)
 
+
 def test_no_username_and_password():
     foo = Mantis()
+
 
 def test_sourcefile_float():
     with pytest.raises(TypeError):
         foo = Mantis(sourcefile=2.3)
 
+
 def test_nonexistent_sourcefile():
     with pytest.raises(ValueError):
         foo = Mantis(sourcefile='/i/do/not/exit')
+
 
 def test_sourcefile():
     foo = Mantis(sourcefile='./tests/minimal.netxml')
