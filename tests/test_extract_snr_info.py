28c5b5af837647aa27ab3085b6d4012388ef6372
tpltnt <tpltnt.github@dropcut.net>
2013-03-24 09:07:08 +0100

    snr info pep8 fixes

--- tests/test_extract_snr_info.py
+++ tests/test_extract_snr_info.py
@@ -8,40 +8,40 @@ pytest.fail("not implmenented")
 
 def test_empty(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data()
+        plainmantis.extract_snr_data()
 
 
 def test_float(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data(2.3)
+        plainmantis.extract_snr_data(2.3)
 
 
 def test_int(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data(42)
+        plainmantis.extract_snr_data(42)
 
 
 def test_string(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data('foo')
+        plainmantis.extract_snr_data('foo')
 
 
-def test_minimalfile(plainmantis, et_ssidnode):
-    data = {'max-rate': 54.0, 'essid': 'WLAN',
+def test_minimalfile(plainmantis, et_snr_node):
+    data = {'max-rate': 54.0, 'esnr': 'WLAN',
             'encryption': ['WPA+TKIP', 'WPA+PSK']}
-    assert data == plainmantis.extract_ssid_data(et_ssidnode)
+    assert data == plainmantis.extract_snr_data(et_snr_node)
 
 
 def test_emptyfile(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data('./tests/empty.xml')
+        plainmantis.extract_snr_data('./tests/empty.xml')
 
 
 def test_empty_et(plainmantis, et_empty):
     with pytest.raises(TypeError):
-        plainmantis.extract_ssid_data(et_empty)
+        plainmantis.extract_snr_data(et_empty)
 
 
 def test_empty_et(plainmantis, et_one_node):
     with pytest.raises(AttributeError):
-        plainmantis.extract_ssid_data(et_one_node)
+        plainmantis.extract_snr_data(et_one_node)
