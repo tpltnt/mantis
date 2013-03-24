c4fded49447eeebc609f149898a1a798b566d5e4
tpltnt <tpltnt.github@dropcut.net>
2013-03-24 09:11:33 +0100

    pep8 fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,11 @@ sys.path.append('../mantis')
 import pytest
 from kismetlogparser import *
 
+
 @pytest.fixture(scope="module")
 def plainmantis():
     return Mantis()
+
 
 @pytest.fixture(scope="module")
 def et_networknode():
@@ -17,22 +19,27 @@ def et_networknode():
     root = tree.getroot()
     return tree.findall('wireless-network')[1]
 
+
 @pytest.fixture(scope="module")
 def et_ssidnode(et_networknode):
     return et_networknode.find('SSID')
+
 
 @pytest.fixture(scope="module")
 def et_gps_node(et_networknode):
     return et_networknode.find('gps-info')
 
+
 @pytest.fixture(scope="module")
 def et_snr_node(et_networknode):
     return et_networknode.find('snr-info')
+
 
 @pytest.fixture(scope="module")
 def et_empty():
     tree = ET.ElementTree()
     return tree.getroot()
+
 
 @pytest.fixture(scope="module")
 def et_one_node():
