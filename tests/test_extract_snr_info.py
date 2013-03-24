7dc57c64b662b980ab30c370509e7b1bd8233e36
tpltnt <tpltnt.github@dropcut.net>
2013-03-24 10:42:26 +0100

    snr tests added

--- tests/test_extract_snr_info.py
+++ tests/test_extract_snr_info.py
@@ -3,45 +3,45 @@ sys.path.append('../mantis')
 import pytest
 from kismetlogparser import *
 
-pytest.fail("not implmenented")
-
 
 def test_empty(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_snr_data()
+        plainmantis.extract_snr_info()
 
 
 def test_float(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_snr_data(2.3)
+        plainmantis.extract_snr_info(2.3)
 
 
 def test_int(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_snr_data(42)
+        plainmantis.extract_snr_info(42)
 
 
 def test_string(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_snr_data('foo')
+        plainmantis.extract_snr_info('foo')
 
 
 def test_minimalfile(plainmantis, et_snr_node):
-    data = {'max-rate': 54.0, 'esnr': 'WLAN',
-            'encryption': ['WPA+TKIP', 'WPA+PSK']}
-    assert data == plainmantis.extract_snr_data(et_snr_node)
+    data = {'max_noise_rssi': 0, 'min_noise_dbm': -80,
+            'max_signal_rssi': 0, 'min_signal_dbm': -93,
+            'min_signal_rssi': 824, 'min_noise_rssi': 824,
+            'max_noise_dbm': 39, 'max_signal_dbm': -68}
+    assert data == plainmantis.extract_snr_info(et_snr_node)
 
 
 def test_emptyfile(plainmantis):
     with pytest.raises(TypeError):
-        plainmantis.extract_snr_data('./tests/empty.xml')
+        plainmantis.extract_snr_info('./tests/empty.xml')
 
 
 def test_empty_et(plainmantis, et_empty):
     with pytest.raises(TypeError):
-        plainmantis.extract_snr_data(et_empty)
+        plainmantis.extract_snr_info(et_empty)
 
 
 def test_empty_et(plainmantis, et_one_node):
     with pytest.raises(AttributeError):
-        plainmantis.extract_snr_data(et_one_node)
+        plainmantis.extract_snr_info(et_one_node)
